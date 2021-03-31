468c4ef0ef4d0fbb5297bf7f36d20ca6fb661c68
Erdem <munkueveg@gmail.com>
2021-04-01 01:17:17 +0800

    Did parent class

--- homework.py
+++ homework.py
@@ -1,8 +1,8 @@
 import datetime as dt
 
+today = dt.date.today().strftime("%d.%m.%Y")
 
 class Record:
-    today = dt.date.today().strftime("%d.%m.%Y")
     def __init__(self, amount, comment, date=today):
         self.amount = amount
         self.comment = comment
@@ -15,7 +15,23 @@ class Calculator:
     def add_record(self, record):
         self.records.append(record)
     def get_today_stats(self):
-        return sum(record.amount for record in self.records)
+        sum = 0
+        for record in self.records:
+            if record.date == today:
+                sum += record.amount
+        return sum
+        #return sum(record.amount for record in self.records)
+    def get_weeks_stats(self):
+        sum = 0
+        today = dt.date.today()
+        week = dt.timedelta(weeks=1)
+        for record in self.records:
+            dateString = record.date
+            dateFormatter = "%d.%m.%Y"
+            dateRecord = dt.datetime.strptime(dateString, dateFormatter)
+            if dateRecord.date() + week >= today:
+                sum += record.amount
+        return sum
 
 #class CaloriesCalculator:
 #    ...
@@ -39,6 +55,7 @@ cash_calculator.add_record(Record(amount=3000,
                                   date='31.03.2021'))
 
 print(cash_calculator.get_today_stats())
+print(cash_calculator.get_weeks_stats())
 #print(cash_calculator.get_today_cash_remained('rub'))
 # должно напечататься
 # На сегодня осталось 555 руб
